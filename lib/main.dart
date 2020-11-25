860baf9824a922e5d9ab398e76f910c098183ece
Schäffer <jnschaff@stud.ntnnu.no>
2020-11-25 17:25:25 +0100

    finalized mi_card

--- lib/main.dart
+++ lib/main.dart
@@ -6,34 +6,68 @@ class MiCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: SafeArea(
-        child: Column(
-          children: [
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: AssetImage('assets/man.jpg'),
-            ),
-            Text(
-              'Grumpy Dude',
-              style: TextStyle(
-                fontSize: 40,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                child: CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('assets/man.jpg'),
+                ),
               ),
-            ),
-            Text(
-              'THE GRUMPER',
-              style: TextStyle(
-                fontSize: 25,
-                color: Colors.teal[100],
-                letterSpacing: 1,
+              Text(
+                'Grumpy Dude',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'THE GRUMPER',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.teal[100],
+                  letterSpacing: 0.8,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  thickness: 2,
+                  color: Colors.white30,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                  ),
+                  title: Text('412 34 567'),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                  ),
+                  title: Text('grumpy@grumper.com'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
