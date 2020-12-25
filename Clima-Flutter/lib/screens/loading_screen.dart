07c2937c14499a7549b6c2346aed251d14f94117
Schäffer <jnschaff@stud.ntnnu.no>
2020-12-25 22:55:42 +0100

    location works

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +7,17 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  LocationData location;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: () {
+          onPressed: () async {
             //Get the current location
+            location = await Location().getLocation();
+            print(location);
           },
           child: Text('Get Location'),
         ),
